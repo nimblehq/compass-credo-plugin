3b491679f69c3a32c57a1513d3d476b45a922bb1
Liam Stevens <liamstevens111@users.noreply.github.com>
2022-09-23 15:13:28 +0700

    Add negative test

--- test/check/do_single_expression_test.exs
+++ test/check/do_single_expression_test.exs
@@ -3,7 +3,7 @@ defmodule CompassCredoPlugin.Check.DoSingleExpressionTest do
 
   alias CompassCredoPlugin.Check.DoSingleExpression
 
-  describe "when there is any functions or if statements with a single line in the body" do
+  describe "given two valid functions BUT the last IF statement contains a single expression" do
     test "reports an issue" do
       module_source_code = """
       defmodule CredoSampleModule do
@@ -27,6 +27,32 @@ defmodule CompassCredoPlugin.Check.DoSingleExpressionTest do
       |> to_source_file()
       |> run_check(DoSingleExpression)
       |> assert_issue()
+    end
+  end
+
+  describe "given two valid functions and a valid IF statement" do
+    test "does NOT report an issue" do
+      module_source_code = """
+      defmodule CredoSampleModule do
+        alias CredoSampleModule.AnotherModule
+
+        def some_function() do
+           a = 5 + 7
+           a + 5
+        end
+
+        def some_other_function(), do: :ok
+
+        if some_condition,
+          do: :ok
+
+      end
+      """
+
+      module_source_code
+      |> to_source_file()
+      |> run_check(DoSingleExpression)
+      |> refute_issues()
     end
   end
 end
