7642634863e696bc21d6936e9fc35be6896eea53
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-10-23 08:29:45 +0700

    Update edge case

--- lib/check/do_single_expression.ex
+++ lib/check/do_single_expression.ex
@@ -73,32 +73,18 @@ defmodule CompassCredoPlugin.Check.DoSingleExpression do
        when definition_type in @matching_definition_types do
     cond do
       contains_single_expression?(body) and contains_do_and_end?(meta) ->
-        if total_body_lines(meta) < @min_required_do_end_body_lines,
-          do:
-            {ast,
-             [
-               issue_for(
-                 meta[:line],
-                 "Single expression and single line in a do ... end block. Use do: instead",
-                 issue_meta
-               )
-               | issues
-             ]},
-          else: {ast, issues}
+        if total_body_lines(meta) < @min_required_do_end_body_lines do
+          {ast, [issue_for(meta[:line], do_end_check_message(), issue_meta) | issues]}
+        else
+          {ast, issues}
+        end
 
       contains_single_expression?(body) and !contains_do_and_end?(meta) ->
-        if total_do_lines(body) > @max_permitted_do_body_lines,
-          do:
-            {ast,
-             [
-               issue_for(
-                 meta[:line],
-                 "Single line in a do: block. Use do ... end block instead",
-                 issue_meta
-               )
-               | issues
-             ]},
-          else: {ast, issues}
+        if total_do_lines(body) > @max_permitted_do_body_lines do
+          {ast, [issue_for(meta[:line], do_check_message(), issue_meta) | issues]}
+        else
+          {ast, issues}
+        end
 
       true ->
         {ast, issues}
@@ -123,6 +109,12 @@ defmodule CompassCredoPlugin.Check.DoSingleExpression do
 
   defp total_do_lines([{{_, do_meta, _}, {_, body_meta, _}}]),
     do: (body_meta[:closing][:line] || body_meta[:line]) - do_meta[:line]
+
+  defp do_end_check_message,
+    do: "Single expression and single line in a do ... end block. Use do: instead"
+
+  defp do_check_message,
+    do: "Multiple lines in a do: block. Use do ... end block instead"
 
   defp issue_for(line_no, message, issue_meta) do
     format_issue(
